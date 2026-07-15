2710bd08cdc3ec9ab7849b5f58aa152f3dac28e7
nahinAlam101 <nahinalam023@gmail.com>
2026-07-16 05:21:28 +0600

    added fxml dashboard file & controller

---
+++ src/main/java/c213/dosaoopproject/User_3_DashboardView_Controller.java
@@ -0,0 +1,102 @@
+package c213.dosaoopproject;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class User_3_DashboardView_Controller
+{
+    @javafx.fxml.FXML
+    private Label pendingCardLabel;
+    @javafx.fxml.FXML
+    private Label recent_FourLabel;
+    @javafx.fxml.FXML
+    private Label msgWelcomeLabel;
+    @javafx.fxml.FXML
+    private Label recentTime_TwoLabel;
+    @javafx.fxml.FXML
+    private Label recentTime_ThreeLabel;
+    @javafx.fxml.FXML
+    private Label hoursCardLabel;
+    @javafx.fxml.FXML
+    private Label recentTime_OneLabel;
+    @javafx.fxml.FXML
+    private Hyperlink viewAllHyperLink;
+    @javafx.fxml.FXML
+    private ImageView ppImageView;
+    @javafx.fxml.FXML
+    private Label titleWelcomeLabel;
+    @javafx.fxml.FXML
+    private Label crtfStatusCardLabel;
+    @javafx.fxml.FXML
+    private Label participationCardLabel;
+    @javafx.fxml.FXML
+    private Label recent_ThreeLabel;
+    @javafx.fxml.FXML
+    private Label recent_OneLabel;
+    @javafx.fxml.FXML
+    private Label recent_TwoLabel;
+    @javafx.fxml.FXML
+    private Label recentTime_FourLabel;
+    @javafx.fxml.FXML
+    private Label nameLabel;
+    @javafx.fxml.FXML
+    private Label userIdLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void certificateApplyOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void campaignsViewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void contributionViewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notificationViewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void menuBarOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void registerViewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void teamViewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void feedbackShareOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void leaveRqstViewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboardViewOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewAllNotificationLinkOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportViewOA(ActionEvent actionEvent) {
+    }
+}
