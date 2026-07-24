fd9ff0f8b80784827c131d23bb7b13d9531194a6
Esha <2430898@iub.edu.bd>
2026-07-25 04:32:18 +0600

    init commit

---
+++ src/main/java/c213/dosaoopproject/esha/controller/u5_G1_DashboardView.java
@@ -0,0 +1,7 @@
+package c213.dosaoopproject.esha.controller;
+
+public class u5_G1_DashboardView
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/c213/dosaoopproject/esha/controller/u5_G2_DashboardView.java
@@ -0,0 +1,7 @@
+package c213.dosaoopproject.esha.controller;
+
+public class u5_G2_DashboardView
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
