28edb7745aad1f34c37784d007707781a1a4ceb1
fahmi739 <2521807@iub.edu.bd>
2026-07-26 00:44:49 +0600

    Notice panel created

---
+++ src/main/java/c213/dosaoopproject/fahmida/U1G1_ViewNotices_overviewController.java
@@ -0,0 +1,28 @@
+package c213.dosaoopproject.fahmida;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class U1G1_ViewNotices_overviewController
+{
+    @javafx.fxml.FXML
+    private TableColumn noticeTitleCol;
+    @javafx.fxml.FXML
+    private TableColumn datePostedCol;
+    @javafx.fxml.FXML
+    private TableColumn postedByCol;
+    @javafx.fxml.FXML
+    private TableColumn categoryCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void readMoreOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOA(ActionEvent actionEvent) {
+    }
+}
