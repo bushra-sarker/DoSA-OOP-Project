1d3f2133c452994ea0dbf3f066167b705d7dc081
fahmi739 <2521807@iub.edu.bd>
2026-07-26 01:08:25 +0600

    Register For Event

---
+++ src/main/java/c213/dosaoopproject/fahmida/U1G2_RegisterForEvents_overviewController.java
@@ -0,0 +1,31 @@
+package c213.dosaoopproject.fahmida;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class U1G2_RegisterForEvents_overviewController
+{
+    @javafx.fxml.FXML
+    private TextField studentNameTF;
+    @javafx.fxml.FXML
+    private TextField studentIDTF;
+    @javafx.fxml.FXML
+    private TextField deptNameTF;
+    @javafx.fxml.FXML
+    private DatePicker dateandVenueDP;
+    @javafx.fxml.FXML
+    private TextField eventNameTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmRegOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backtoDashboardOA(ActionEvent actionEvent) {
+    }
+}
