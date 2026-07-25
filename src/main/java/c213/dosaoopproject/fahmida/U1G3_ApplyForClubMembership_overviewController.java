eac7c117d23ce78b788e50afbe54e02b9611ea15
fahmi739 <2521807@iub.edu.bd>
2026-07-26 01:52:21 +0600

    apply for club membership form

---
+++ src/main/java/c213/dosaoopproject/fahmida/U1G3_ApplyForClubMembership_overviewController.java
@@ -0,0 +1,26 @@
+package c213.dosaoopproject.fahmida;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class U1G3_ApplyForClubMembership_overviewController
+{
+    @javafx.fxml.FXML
+    private TextField studentNameTF;
+    @javafx.fxml.FXML
+    private TextField relevantSkillsTF;
+    @javafx.fxml.FXML
+    private TextField whyJoinTF;
+    @javafx.fxml.FXML
+    private TextField clubNameTF;
+    @javafx.fxml.FXML
+    private TextField majorTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void submitApplicationOA(ActionEvent actionEvent) {
+    }
+}
