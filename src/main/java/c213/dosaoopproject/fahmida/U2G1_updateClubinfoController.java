4bc03596cf6c2bfcad7212bd68a50859211cf8b0
fahmi739 <2521807@iub.edu.bd>
2026-07-27 02:49:38 +0600

    update club info pages created

---
+++ src/main/java/c213/dosaoopproject/fahmida/U2G1_updateClubinfoController.java
@@ -0,0 +1,82 @@
+package c213.dosaoopproject.fahmida;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class U2G1_updateClubinfoController
+{
+    @javafx.fxml.FXML
+    private Label welcomeUserLabel;
+    @javafx.fxml.FXML
+    private TextField phoneTF;
+    @javafx.fxml.FXML
+    private TextField emailTF;
+    @javafx.fxml.FXML
+    private Label userIdLabel11;
+    @javafx.fxml.FXML
+    private TextField scheduleTF;
+    @javafx.fxml.FXML
+    private TextField descriptionTF;
+    @javafx.fxml.FXML
+    private ImageView ppImageView11;
+    @javafx.fxml.FXML
+    private Label DOSALabel;
+    @javafx.fxml.FXML
+    private TextField venueTF;
+    @javafx.fxml.FXML
+    private Label nameLabel11;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerEventsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notificationOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewScheduleOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void communityProgramOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewNoticesOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveChangesOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadApprovalOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clubMembershipOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void studentdashboardOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackHistoryOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitComplaintsOA(ActionEvent actionEvent) {
+    }
+}
