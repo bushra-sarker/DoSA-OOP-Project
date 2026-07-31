dd7bdf27b05090f75cf355cb2d35969bccf90c71
fahmi739 <2521807@iub.edu.bd>
2026-08-01 01:33:20 +0600

    View event schedule pagre created

---
+++ src/main/java/c213/dosaoopproject/fahmida/U1G4_ViewEventScheduleController.java
@@ -0,0 +1,87 @@
+package c213.dosaoopproject.fahmida;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+
+public class U1G4_ViewEventScheduleController
+{
+    @javafx.fxml.FXML
+    private Label userIdLabel11;
+    @javafx.fxml.FXML
+    private ImageView ppImageView11;
+    @javafx.fxml.FXML
+    private Label DOSALabel;
+    @javafx.fxml.FXML
+    private Label nameLabel11;
+    @javafx.fxml.FXML
+    private TableColumn timeTC;
+    @javafx.fxml.FXML
+    private TableColumn activityNameTC;
+    @javafx.fxml.FXML
+    private TableColumn venueTC;
+    @javafx.fxml.FXML
+    private TableView EventScheduleTV;
+    @javafx.fxml.FXML
+    private TableColumn dateTC;
+    @javafx.fxml.FXML
+    private TableColumn organizingClubTC;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerEventsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notificationOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewScheduleOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void communityProgramOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewNoticesOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadApprovalOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clubMembershipOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void studentdashboardOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackHistoryOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitComplaintsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backtoSchedulesOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void gotoDashboardOA(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/c213/dosaoopproject/fahmida/U1G5_Enrollincommunity_overviewController.java
+++
@@ -1,7 +0,0 @@
-package c213.dosaoopproject.fahmida;
-
-public class U1G5_Enrollincommunity_overviewController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
