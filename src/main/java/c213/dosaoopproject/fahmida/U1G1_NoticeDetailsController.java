f49b0dcc412d89a8b14326bf5b3904b313b866bb
fahmi739 <2521807@iub.edu.bd>
2026-08-02 02:39:34 +0600

    U1G1 edited

---
+++ src/main/java/c213/dosaoopproject/fahmida/U1G1_NoticeDetailsController.java
@@ -0,0 +1,67 @@
+package c213.dosaoopproject.fahmida;
+
+public class U1G1_NoticeDetailsController
+{
+    @javafx.fxml.FXML
+    private Label viewNoticeLabel;
+    @javafx.fxml.FXML
+    private Label userIdLabel11;
+    @javafx.fxml.FXML
+    private ImageView ppImageView11;
+    @javafx.fxml.FXML
+    private Label DOSALabel;
+    @javafx.fxml.FXML
+    private Label nameLabel11;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerEventsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notificationOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewScheduleOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void communityProgramOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewNoticesOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadApprovalOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clubMembershipOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goDashboardOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void studentdashboardOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackHistoryOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitComplaintsOA(ActionEvent actionEvent) {
+    }
+}
