0c055c5d627874382964c3b0d171e5c35e71ef8d
fahmi739 <2521807@iub.edu.bd>
2026-08-01 21:49:05 +0600

    created

---
+++ src/main/java/c213/dosaoopproject/fahmida/U2G3_ReviewandapproveClubMembershipController.java
@@ -0,0 +1,74 @@
+package c213.dosaoopproject.fahmida;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.image.ImageView;
+
+public class U2G3_ReviewandapproveClubMembershipController
+{
+    @javafx.fxml.FXML
+    private Label userIdLabel11;
+    @javafx.fxml.FXML
+    private ImageView ppImageView11;
+    @javafx.fxml.FXML
+    private Label DOSALabel;
+    @javafx.fxml.FXML
+    private Label nameLabel11;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerEventsOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notificationOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewScheduleOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void communityProgramOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewNoticesOA(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void createNoticeButtonOA(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void backtoDashboardButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadApprovalOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clubMembershipOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void studentdashboardOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackHistoryOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitComplaintsOA(ActionEvent actionEvent) {
+    }
+}
