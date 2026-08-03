be7efefa32b33de0ec8adf7e7ff5a1130a7f58de
bushra-sarker <bushra.sarker.07@gmail.com>
2026-08-03 22:57:27 +0600

    create SubViewSwitcher class in util

--- src/main/java/commonClass/util/SubViewSwitcher.java
+++ src/main/java/commonClass/util/SubViewSwitcher.java
@@ -1,42 +1,28 @@
 package commonClass.util;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.layout.StackPane;
-
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
 import java.io.IOException;
 
 public class SubViewSwitcher {
 
-//     Loads an FXML file and replaces the content of the specified StackPane container.
-//     container The StackPane host where the view should be displayed.
-//     fxmlPath  The absolute path to the subview FXML resource.
+//     Swaps the content inside a container Pane: (e.g., center Pane of main layout).
+//     container The target Pane: (VBox, StackPane, BorderPane center, etc.)
+//     Full path to resource: e.g., "/c213/dosaoopproject/Bushra/U08/U08G8_feedbackAnalytics.fxml"
+//     controller attached to the loaded view (optional)
 
-    public static <T> T loadSubView(StackPane container, String fxmlPath) {
-        if (container == null) {
-            System.err.println("SubViewSwitcher Error: Container StackPane is null.");
-            return null;
-        }
-
+    public static Object loadSubView(Pane container, String fxmlPath) {
         try {
             FXMLLoader loader = new FXMLLoader(SubViewSwitcher.class.getResource(fxmlPath));
-            Parent view = loader.load();
-
+            Node subView = loader.load();
             container.getChildren().clear();
-            container.getChildren().add(view);
-
+            container.getChildren().add(subView);
             return loader.getController();
         } catch (IOException e) {
-            System.err.println("SubViewSwitcher Error: Could not load FXML at path " + fxmlPath);
             e.printStackTrace();
+            AlertUtil.showError("UI Navigation Error", "Unable to load sub-view: " + fxmlPath);
             return null;
-        }
-    }
-
-
-    public static void clearContainer(StackPane container) {
-        if (container != null) {
-            container.getChildren().clear();
         }
     }
 }
