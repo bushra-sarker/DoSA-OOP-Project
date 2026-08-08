4cc47f398cfbe6940845bf5473a6932237af853a
Esha <2430898@iub.edu.bd>
2026-08-09 04:49:37 +0600

    init commit

---
+++ src/main/java/c213/dosaoopproject/esha/model/ClubExecutive.java
@@ -0,0 +1,44 @@
+package c213.dosaoopproject.esha.model;
+
+public class ClubExecutive {
+
+    // Goal 1
+    public void arrangeClubEvent() {
+        // Implementation for arranging a club event
+    }
+
+    // Goal 2
+    public void submitEventRequest() {
+        // Implementation for submitting an event request
+    }
+
+    // Goal 3
+    public void bookVenue() {
+        // Implementation for booking a venue
+    }
+
+    // Goal 4
+    public void submitSponsorshipRequest() {
+        // Implementation for submitting a sponsorship request
+    }
+
+    // Goal 5
+    public void submitResourceRequest() {
+        // Implementation for submitting a resource request
+    }
+
+    // Goal 6
+    public void postNotice() {
+        // Implementation for posting a notice
+    }
+
+    // Goal 7
+    public void reviewMembershipApplications() {
+        // Implementation for reviewing membership applications
+    }
+
+    // Goal 8
+    public void submitEventCompletionReport() {
+        // Implementation for submitting an event completion report
+    }
+}
---
+++ src/main/java/c213/dosaoopproject/esha/model/DoSACoordinator.java
@@ -0,0 +1,44 @@
+package c213.dosaoopproject.esha.model;
+
+public class DoSACoordinator {
+
+    // Goal 1
+    public void postServiceOpportunity() {
+        // Implementation for posting a service opportunity
+    }
+
+    // Goal 2
+    public void approveVolunteerHours() {
+        // Implementation for approving volunteer hours
+    }
+
+    // Goal 3
+    public void assignVolunteers() {
+        // Implementation for assigning volunteers
+    }
+
+    // Goal 4
+    public void generateCertificate() {
+        // Implementation for generating a certificate
+    }
+
+    // Goal 5
+    public void reviewEventFeedback() {
+        // Implementation for reviewing event feedback
+    }
+
+    // Goal 6
+    public void budgetApproval() {
+        // Implementation for budget approval
+    }
+
+    // Goal 7
+    public void generateImpactReport() {
+        // Implementation for generating an impact report
+    }
+
+    // Goal 8
+    public void volunteerTaskCompletionReport() {
+        // Implementation for volunteer task completion report
+    }
+}
