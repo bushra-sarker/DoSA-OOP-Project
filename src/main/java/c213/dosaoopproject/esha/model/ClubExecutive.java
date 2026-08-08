8513bf8444c65085fd3c8bb495c511d38250162a
Esha <2430898@iub.edu.bd>
2026-08-09 05:16:18 +0600

    Update model classes with proper method parameters for User 5 and User 6 goals

--- src/main/java/c213/dosaoopproject/esha/model/ClubExecutive.java
+++ src/main/java/c213/dosaoopproject/esha/model/ClubExecutive.java
@@ -1,44 +1,46 @@
 package c213.dosaoopproject.esha.model;
 
+import java.time.LocalDate;
+
 public class ClubExecutive {
 
     // Goal 1
-    public void arrangeClubEvent() {
+    public void arrangeClubEvent(String eventName, String description, LocalDate eventDate, String venue, String status) {
         // Implementation for arranging a club event
     }
 
     // Goal 2
-    public void submitEventRequest() {
+    public void submitEventRequest(String eventDescription, LocalDate eventDate) {
         // Implementation for submitting an event request
     }
 
     // Goal 3
-    public void bookVenue() {
+    public void bookVenue(String venueName, LocalDate bookingDate, String startTime, String endTime) {
         // Implementation for booking a venue
     }
 
     // Goal 4
-    public void submitSponsorshipRequest() {
+    public void submitSponsorshipRequest(String eventName, String sponsorshipType, String sponsorOrgName, String expectedSupport) {
         // Implementation for submitting a sponsorship request
     }
 
     // Goal 5
-    public void submitResourceRequest() {
+    public void submitResourceRequest(String resourceCategory, String materialName, int quantity, LocalDate requiredDate, String purpose) {
         // Implementation for submitting a resource request
     }
 
     // Goal 6
-    public void postNotice() {
+    public void postNotice(String title, String body) {
         // Implementation for posting a notice
     }
 
     // Goal 7
-    public void reviewMembershipApplications() {
+    public void reviewMembershipApplications(int applicationId, boolean isApproved) {
         // Implementation for reviewing membership applications
     }
 
     // Goal 8
-    public void submitEventCompletionReport() {
+    public void submitEventCompletionReport(String eventName, int attendanceCount, String outcomeSummary) {
         // Implementation for submitting an event completion report
     }
 }
--- src/main/java/c213/dosaoopproject/esha/model/DoSACoordinator.java
+++ src/main/java/c213/dosaoopproject/esha/model/DoSACoordinator.java
@@ -1,44 +1,46 @@
 package c213.dosaoopproject.esha.model;
 
+import java.time.LocalDate;
+
 public class DoSACoordinator {
 
     // Goal 1
-    public void postServiceOpportunity() {
+    public void postServiceOpportunity(String title, String description, LocalDate date, String location, int availableSlots) {
         // Implementation for posting a service opportunity
     }
 
     // Goal 2
-    public void approveVolunteerHours() {
+    public void approveVolunteerHours(int approvalId) {
         // Implementation for approving volunteer hours
     }
 
     // Goal 3
-    public void assignVolunteers() {
+    public void assignVolunteers(int approvalId) {
         // Implementation for assigning volunteers
     }
 
     // Goal 4
-    public void generateCertificate() {
+    public void generateCertificate(int volunteerId) {
         // Implementation for generating a certificate
     }
 
     // Goal 5
-    public void reviewEventFeedback() {
+    public void reviewEventFeedback(int feedbackId, String action, String remarks) {
         // Implementation for reviewing event feedback
     }
 
     // Goal 6
-    public void budgetApproval() {
+    public void budgetApproval(int budgetId, boolean isApproved, String remarks) {
         // Implementation for budget approval
     }
 
     // Goal 7
-    public void generateImpactReport() {
+    public void generateImpactReport(String semester, String year, String executiveSummary) {
         // Implementation for generating an impact report
     }
 
     // Goal 8
-    public void volunteerTaskCompletionReport() {
+    public void volunteerTaskCompletionReport(int assignmentId, String completionStatus, int hoursWorked, String remarks) {
         // Implementation for volunteer task completion report
     }
 }
